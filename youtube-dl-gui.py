d9b04edffb84065a85475ce00ca412d956742beb
Md. Minhazul Haque <1847242+mdminhazulhaque@users.noreply.github.com>
2019-04-09 12:40:36 +0800

    Update youtube-dl-gui.py

--- youtube-dl-gui.py
+++ youtube-dl-gui.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+__author__ = "Md. Minhazul Haque"
+__license__ = "GPLv3"
+
+"""
+Copyright (c) 2018 Md. Minhazul Haque
+This file is part of mdminhazulhaque/bd-mrp-api
+(see https://github.com/mdminhazulhaque/banglalionwimaxapi).
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
 from PyQt5.QtWidgets import (QWidget, QLabel, QLineEdit, QPushButton, QProgressBar, QApplication, QVBoxLayout, QMessageBox)
 from PyQt5.QtCore import QProcess
 import sys
